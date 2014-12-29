4c04ae72ba3f88e4b1f187147d6234294de1f889
johna1203 <johnathan@kodokuman.com>
2014-12-29 12:33:18 -0200

    init

---
+++ pages/home/HomeCtrl.js
@@ -0,0 +1,8 @@
+/**
+*/
+
+'use strict';
+
+angular.module('myApp').controller('HomeCtrl', ['$scope', 'geekylab/angular-geekymenuGeekymenu', function($scope, geekylab/angular-geekymenuGeekymenu) {
+	//TODO - put any directive code here
+}]);
