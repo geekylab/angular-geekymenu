b928589c733ceaa4fe18f06cb89183c49efe2f97
johna1203 <johnathan@kodokuman.com>
2014-12-29 12:54:56 -0200

    orderItem:accept:response event for client.

--- pages/home/HomeCtrl.js
+++
@@ -1,8 +0,0 @@
-/**
-*/
-
-'use strict';
-
-angular.module('myApp').controller('HomeCtrl', ['$scope', 'geekylab/angular-geekymenuGeekymenu', function($scope, geekylab/angular-geekymenuGeekymenu) {
-	//TODO - put any directive code here
-}]);
