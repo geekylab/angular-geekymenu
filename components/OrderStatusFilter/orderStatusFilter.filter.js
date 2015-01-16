f87fcb309d1d381384bba4a631c7e3408c2e4d5e
johna1203 <johnathan@kodokuman.com>
2015-01-16 17:45:46 -0200

    Order detail page.

--- components/OrderStatusFilter/orderStatusFilter.filter.js
+++ components/OrderStatusFilter/orderStatusFilter.filter.js
@@ -1,13 +1,24 @@
 'use strict';
 angular.module('gl.geekymenu')
     .filter('orderStatusFilter', function ($translate, mySettings) {
-        return function (input) {
-            if (input == mySettings.ORDER_STATUS_AUTHENTICATED) {
-                return $translate.instant("AUTHENTICATED");
-            } else if (input == mySettings.ORDER_STATUS_AUTHENTICATION_WAITING) {
-                return $translate.instant("AUTHENTICATED WAITING");
-            } else if (input == mySettings.ORDER_STATUS_REJECTED) {
-                return $translate.instant("REJECTED");
+        return function (input, type) {
+
+            if (type == undefined) {
+                if (input == mySettings.ORDER_STATUS_AUTHENTICATED) {
+                    return $translate.instant("AUTHENTICATED");
+                } else if (input == mySettings.ORDER_STATUS_AUTHENTICATION_WAITING) {
+                    return $translate.instant("AUTHENTICATED WAITING");
+                } else if (input == mySettings.ORDER_STATUS_REJECTED) {
+                    return $translate.instant("REJECTED");
+                }
+            } else if (type == 'orderItem') {
+                if (input == mySettings.ITEM_STATUS_MAKING) {
+                    return $translate.instant("MAKING");
+                } else if (input == mySettings.ITEM_STATUS_COMPLETE) {
+                    return $translate.instant("COMPLETE");
+                } else if (input == mySettings.ITEM_STATUS_CANCEL) {
+                    return $translate.instant("CANCELED");
+                }
             }
             return $translate.instant("UNKNOWN");
         };
--- components/mySettings/mySettings.service.js
+++ components/mySettings/mySettings.service.js
@@ -4,8 +4,12 @@ angular.module('cloud2App')
     .factory('mySettings', function () {
         // Public API here
         return {
-            ORDER_STATUS_AUTHENTICATED: 1,
-            ORDER_STATUS_AUTHENTICATION_WAITING: 2,
-            ORDER_STATUS_REJECTED: 99
+            ORDER_STATUS_AUTHENTICATION_WAITING: 1,
+            ORDER_STATUS_AUTHENTICATED: 2,
+            ORDER_STATUS_REJECTED: 99,
+
+            ITEM_STATUS_MAKING: 1,
+            ITEM_STATUS_COMPLETE: 2,
+            ITEM_STATUS_CANCEL: 99
         };
     });
